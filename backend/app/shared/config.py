1871aac4622914a9291bbcc17451ed9e9d7afea5
Jose Gurtubay <gurtubay.dev@gmail.com>
2024-11-25 22:44:31 -0600

    Conexion mongo docker

--- backend/app/shared/config.py
+++ backend/app/shared/config.py
@@ -1,7 +1,7 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    # MONGO_URI: str = "mongodb://database:27017"
-    MONGO_URI: str = "mongodb://localhost:27017/movie_club"
+    MONGO_URI: str = "mongodb://database:27017"
+    # MONGO_URI: str = "mongodb://localhost:27017/movie_club"
 
 settings = Settings()
